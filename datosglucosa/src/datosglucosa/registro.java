9d28f8fceba9fc98419a36aee46c686e8d0bde54
adria <irdaale1125@gmail.com>
2026-04-24 13:30:32 -0600

    en la clase registro se agrega la función fecha con LocalDate

--- datosglucosa/src/datosglucosa/registro.java
+++ datosglucosa/src/datosglucosa/registro.java
@@ -1,19 +1,25 @@
 package datosglucosa;
-
+import java.time.LocalDate;
 
 public class registro {
 	private String nombre;
 	private double glucosa; 
-
+	private LocalDate fecha;
 	public registro() {}
 		public registro(String nombre, double glucosa) {
 			this.nombre= nombre;
 			this.glucosa= glucosa;
-			
+			this.fecha= fecha;
 		 
 		
 		}
 
+		public LocalDate getFecha() {
+			return fecha;
+		}
+		public void setFecha(LocalDate fecha) {
+			this.fecha = fecha;
+		}
 		public String getNombre() {
 			return nombre;
 		}
